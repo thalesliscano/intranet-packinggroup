4106b6900c75e09b78160a7cdb3a903d45ec2fdb
Thales Carvalho <thales.carvalho@PACKINGGROUP.NET>
2024-11-22 13:55:06 -0400

    feat: atualização e adição de novos componentes
    - Removidos: AppsCarousel.vue e BannerSeguranca.vue.
    - Atualizado: ListaApps.vue, com ajustes no nome, adição de novos itens, links e ícones, restando apenas os de Zabbix e Redmine.
    - Adicionado: DicasGeral.vue, implementando uma seção tipo vitrine com itens exibidos em janelas. Quando o mouse passa sobre os itens, a imagem muda, e ao clicar no botão, o usuário é redirecionado para outra página.

---
+++ intranet-corporativa/src/components/Home/DicasGeral.vue
@@ -0,0 +1,184 @@
+<template lang="pug">
+.banner-seguranca
+  h1.titulo Dicas de seguranças
+  ul.container-lista-dicas
+    li(v-for="(item, index) in conteudoDicas" :key="index" class="item-dica")
+      .conteudo-dicas(
+        :style="{ backgroundImage: `url(${item.defaultImage})`, backgroundSize: item.backgroundSize, backgroundPosition: item.position }"
+        @mouseenter="atualizarHover(index, true)"
+        @mouseleave="atualizarHover(index, false)"
+      )
+        .conteudo
+          h2.title-dica(v-if="!item.isHovered" class="fade") {{ item.title }}
+          p.descricao-dica(v-else class="fade") {{ item.description }}
+          button.button-dica Clique aqui
+</template>
+  
+  
+  
+<script>
+export default {
+  name: "BannerSeguranca",
+  data() {
+    return {
+      conteudoDicas: [
+        {
+          title: "Titulo 1",
+          description: "Esta é a descrição do item 1 bla bla bla bla bla",
+          defaultImage: require("../../assets/banner-seguranca.jpg"),
+          hoverImage: require("../../assets/img3.png"),
+          backgroundSize: "100% 100%",
+          position: "center",
+          isHovered: false,
+        },
+        {
+          title: "Titulo 2",
+          description: "Esta é a descrição do item 2 sdfd ddkdk thales jhonny lindos",
+          defaultImage: require("../../assets/banner-seguranca.jpg"),
+          hoverImage: require("../../assets/img3.png"),
+          backgroundSize: "100% 100%",
+          position: "center",
+          isHovered: false,
+        },
+        {
+          title: "Titulo 3",
+          description: "Esta é a descrição do item 3 eba kejlje ekjklj kl he lorem ipsum hffj djdj ",
+          defaultImage: require("../../assets/banner-seguranca.jpg"),
+          hoverImage: require("../../assets/img3.png"),
+          backgroundSize: "100% 100%",
+          position: "center",
+          isHovered: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    atualizarHover(index, isHover) {
+      const item = this.conteudoDicas[index];
+      item.defaultImage = isHover ? item.hoverImage : require("../../assets/banner-seguranca.jpg");
+      item.isHovered = isHover;
+    },
+  },
+};
+
+</script>
+  
+  <style lang="scss" scoped>
+  .banner-seguranca {
+    margin: 60px auto;
+    width: 1000px;
+    .titulo{
+      text-align: center;
+      margin-bottom: 30px;
+    }
+    .container-lista-dicas {
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 20px;
+      list-style: none;
+  
+      .item-dica {
+        width: 250px;
+        height: 250px;
+        display: flex;
+        border-radius: 10px;
+        justify-content: center;
+        align-items: center;
+  
+        .conteudo-dicas {
+           border: 3px solid #0f2034;
+          border-radius: 10px;
+          width: 250px;
+          height: 250px;
+          text-align: center;
+          display: flex;
+          flex-direction: column;
+          background-repeat: no-repeat;
+          position: relative;
+          transition: background-image 0.3s ease;
+  
+          &::after {
+            content: "";
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            background-color: #000;
+            opacity: 30%;
+            top: 0px;
+            border-radius: 10px;
+            left: 0px;
+          }
+          .conteudo{
+            z-index: 2;
+            display: flex;
+            flex-direction: column;
+            gap: 50px;
+            .title-dica, .descricao-dica {
+              margin: 0;
+              height: 60px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              overflow: hidden;
+              text-align: center;
+              padding: 30px;
+              color: #fff;
+              font-weight: 500;
+              animation: fadeSlideIn 0.5s ease forwards; /* Animação padrão de entrada */
+
+              &.fade-out {
+                animation: fadeSlideOut 0.5s ease forwards; /* Animação de saída */
+              }
+            }
+
+  
+            .button-dica {
+              align-self: flex-end;
+              margin: 0 auto 0 auto;
+              padding: 10px;
+              border-radius: 10px;
+              border: transparent;
+              background-color: #fff;
+              font-weight: 600;
+              cursor: pointer;
+              &:hover{
+                background-color: #0f2034;
+                color: #fff;
+                border: 1px solid #fff;
+              }
+            }
+
+          }
+        }
+        &:hover{
+        background-color: #0f2034;
+      }
+      }
+    }
+  }
+  @keyframes fadeSlideIn {
+  from {
+    opacity: 0;
+    transform: translateY(10px); /* Desliza para cima */
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0); /* Posição final */
+  }
+}
+
+@keyframes fadeSlideOut {
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateY(-10px); /* Desliza para fora */
+  }
+}
+
+  </style>
+  
