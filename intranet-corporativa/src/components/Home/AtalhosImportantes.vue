5b6eb3bd8439be903f83214eefd6bbb5aa56a445
Thales Carvalho <thales.carvalho@PACKINGGROUP.NET>
2024-11-22 13:48:50 -0400

    fix: ajustes no layout e adição de novos itens com ícones e links
    - Novos ícones adicionados: cadastro.png, explicar.png, gmail.png, icone-documentacao.png, inventario.png, microsoft.png, ponto-de-acesso.png, spam.png, telefone.png.
    - Componente 'AtalhosImportantes.vue' atualizado para incluir novos itens.
    - Pequenos ajustes no layout para melhor alinhamento.

--- intranet-corporativa/src/components/Home/AtalhosImportantes.vue
+++ intranet-corporativa/src/components/Home/AtalhosImportantes.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
 section.atalhos-container
+    h1.titulo Atalhos importantes
     main.lista-atalhos-container
         ul.lista-atalhos-itens
             li(v-for='item in itensAtalhos' :key="item.nome" class="item-atalho" @click="navigateTo(item)").item-atalho 
@@ -24,29 +25,54 @@ export default {
                     img: require("../../assets/ciber-seguranca.png"),
                 },
                 {
-                    nome: "Sistema de Chamado",
-                    link: "https://www.google.com",
-                    img: require("../../assets/logo-glpi-bleu-1.png"),
-                },
-                {
                     nome: "Documentação",
                     link: "/documentacao",
-                    img: require("../../assets/manutencao.png"),
+                    img: require("../../assets/icone-documentacao.png"),
                 },
                 {
-                    nome: "Aletas e Manutenção",
+                    nome: "Lista de ramais",
+                    link: "https://www.google.com",
+                    img: require("../../assets/telefone.png"),
+                },
+                {
+                    nome: "Email",
+                    link: "https://exchange.packtec.com.br/owa/auth/logon.aspx?replaceCurrent=1&url=https%3a%2f%2fexchange.packtec.com.br%2fowa%2f",
+                    img: require("../../assets/gmail.png"),
+                },
+                {
+                    nome: "Acesso ao wifi",
+                    link: "https://www.google.com",
+                    img: require("../../assets/ponto-de-acesso.png"),
+                },
+                {
+                    nome: "Canal de sugestão e denúncia",
+                    link: "https://www.google.com",
+                    img: require("../../assets/explicar.png"),
+                },
+                {
+                    nome: "Alertas e Manutenção",
                     link: "https://www.google.com",
                     img: require("../../assets/manutencao.png"),
                 },
                 {
-                    nome: "Aletas e Manutenção",
-                    link: "https://www.google.com",
-                    img: require("../../assets/manutencao.png"),
+                    nome: "Microsoft",
+                    link: "https://login.microsoftonline.com/common/oauth2/authorize?client_id=00000006-0000-0ff1-ce00-000000000000&response_type=code%20id_token&scope=openid%20profile&state=OpenIdConnect.AuthenticationProperties%3D5PzZIESJAWM-cEhwZ4n5ftFsNL4ktlUI1GauA1AO1n9fxrPfKK2PVmVbm5iri-koLUPkAwUBqVby9fRHatYkhS0X7CG01qRbXH6U5IzFZF6Lah5_VvM4nY6w-2ITzS35_7A29PAzdAJuB6Wj0GZLDNSStPNyue5hWEv_1YSSi-U&response_mode=form_post&nonce=638678838070629908.OTYxNzFlNTYtMWMxOS00YjBhLTk1NDEtYzBmNjJhNzhiOTBkMTc0MTM5MzQtNzViMi00YmZjLTk3YzAtNzUwZjczMWVlYjdk&redirect_uri=https%3A%2F%2Fadmin.microsoft.com%2Flanding&ui_locales=pt-BR&mkt=pt-BR&client-request-id=2dfccbdc-09af-4fac-b6c4-a7df230a34b0&claims=%7B%22id_token%22%3A%7B%22xms_cc%22%3A%7B%22values%22%3A%5B%22CP1%22%5D%7D%7D%7D&x-client-SKU=ID_NET472&x-client-ver=8.1.2.0&sso_reload=true",
+                    img: require("../../assets/microsoft.png"),
                 },
                 {
-                    nome: "Aletas e Manutenção",
-                    link: "https://www.google.com",
-                    img: require("../../assets/manutencao.png"),
+                    nome: "Cadastro",
+                    link: "http://suporte.packinggroup.net/front/central.php",
+                    img: require("../../assets/cadastro.png"),
+                },
+                {
+                    nome: "Inventario",
+                    link: "http://inventario.packinggroup.net/",
+                    img: require("../../assets/inventario.png"),
+                },
+                {
+                    nome: "Anti-spam",
+                    link: "https://spamlogin.com/spampanel.php#/blacklistsender",
+                    img: require("../../assets/spam.png"),
                 },
             ],
         };
@@ -68,7 +94,11 @@ export default {
 
 <style lang="scss">
 .atalhos-container {
-    margin-top: 50px;
+    margin-top: 70px;
+    .titulo{
+        text-align: center;
+        font-size: 32px;
+}
 
     .titulo-atalhos {
         margin-top: 30px;
@@ -78,9 +108,9 @@ export default {
     }
 
     .lista-atalhos-container {
-        width: 1000px;
+        width: 1200px;
         margin: 0 auto;
-        padding: 40px;
+        padding: 20px;
 
         .lista-atalhos-itens {
             display: flex;
@@ -94,10 +124,10 @@ export default {
                 align-items: center;
                 gap: 10px;
                 background-color: #fff;
-                border: 3px solid #0f2034;
+                // border: 3px solid #0f2034;
                 padding: 10px;
-                height: 200px;
-                width: 200px;
+                height: 150px;
+                width: 150px;
                 border-radius: 50%;
                 cursor: pointer;
                 box-sizing: border-box; // Inclui bordas no cálculo de tamanho
@@ -117,17 +147,17 @@ export default {
                     text-align: center;
                     justify-content: center;
                     background-color: transparent; // Fundo transparente para evitar interferência
-                    gap: 5px;
+                    gap: 8px;
 
 
                     .item-nome {
-                        font-size: 20px;
+                        font-size: 15px;
                         color: #000; // Cor padrão do texto
                         transition: color 0.3s ease; // Suaviza a transição de cor
                     }
 
                     .container-img {
-                        width: 40px;
+                        width: 35px;
                         height: 35px;
 
                         .item-img {
