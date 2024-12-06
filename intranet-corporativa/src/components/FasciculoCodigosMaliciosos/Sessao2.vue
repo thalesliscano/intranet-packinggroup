7b572983cda132b98f27386a47a7a46bc55e0821
Thales Carvalho <thales.carvalho@PACKINGGROUP.NET>
2024-12-06 15:27:32 -0400

    add-sessaoDois finalizada

--- intranet-corporativa/src/components/FasciculoCodigosMaliciosos/Sessao2.vue
+++ intranet-corporativa/src/components/FasciculoCodigosMaliciosos/Sessao2.vue
@@ -2,9 +2,9 @@
 .sessao-Dois
     .container-conteudo(v-for="item in items" :key="item")
         h1.titulo-vermelho {{item.textoForteUm}}
-        p.texto {{item.textoFracoUm}}
-        p.texto {{item.textoFracoDois}}
-        h1.titulo-vermelho {{ item.textoForteDois}}
+        p.texto(:class="item.textoFracoUm.selfLayout ? 'position-self-down': ''") {{item.textoFracoUm.texto}}
+        p.texto(:class="item.textoFracoDois.selfLayout ? 'position-self-down': ''") {{item.textoFracoDois.texto}}
+        h1.titulo-vermelho(:class="item.textoForteDois.selfLayout ? 'self-start-end': ''") {{ item.textoForteDois.texto}}
 
 </template>
 
@@ -15,10 +15,16 @@ export default {
         return{
             items: [
                 {
-                    textoForteUm: "CÓDIGOS MALICIOSOS SÃO USADOS COMO INTERMEDIÁRIOS E POSSIBILITAM A PRÁTICADE GOLPES, A REALIZAÇÃO DE ATAQUES E O ENVIO DE SPAM",
-                    textoFracoUm: "acesse mídias removíveis infecta- das, como pen drivesexecute arquivos infectados, obtidos em anexos de mensagens eletrônicas, via mídias remo- víveis, em páginas web, redes sociais ou diretamente de outros equipamentos.Após infectar o seu equipamento, o código malicioso pode executar ações como se fosse você, comoacessar informações, apagar arquivos, criptografar dados, conectar-se à Internet, enviar mensagens e ainda instalar outros códigos maliciosos.A melhor prevenção contra os códigos maliciosos é impedir que a infecção ocorra pois nem sempre é possível reverter as ações danosas já feitas ou recuperar totalmente seus dados.",
-                    textoFracoDois: "Códigos maliciosos, também conhecidos como pragas e malware, são programas desenvolvidos para executar ações danosas e atividades maliciosas em equipamentos, como computadores, modems, switches, roteadores e dispositivos móveis (tablets, celulares, smartphones, etc).Um atacante pode instalar um código malicioso após invadir um equipamento ou explorando alguma vulnerabilidade existente nos programas nele instalados.Seus equipamentos também podem ser infectados caso você:acesse páginas web maliciosas, usando navegadores vulneráveis",
-                    textoForteDois: "CÓDIGOS MALICIOSOS: PROTEJA-SE DESTA TURMA",
+                    textoForteUm : "CÓDIGOS MALICIOSOS SÃO USADOS COMO INTERMEDIÁRIOS E POSSIBILITAM A PRÁTICADE GOLPES, A REALIZAÇÃO DE ATAQUES E O ENVIO DE SPAM",
+                    textoFracoUm: {
+                        texto:"Acesse mídias removíveis infecta- das, como pen drivesexecute arquivos infectados, obtidos em anexos de mensagens eletrônicas, via mídias remo- víveis, em páginas web, redes sociais ou diretamente de outros equipamentos.Após infectar o seu equipamento, o código malicioso pode executar ações como se fosse você, comoacessar informações, apagar arquivos, criptografar dados, conectar-se à Internet, enviar mensagens e ainda instalar outros códigos maliciosos.A melhor prevenção contra os códigos maliciosos é impedir que a infecção ocorra pois nem sempre é possível reverter as ações danosas já feitas ou recuperar totalmente seus dados.", selfLayout: true,
+                    },
+                    textoFracoDois: {
+                        texto:"Códigos maliciosos, também conhecidos como pragas e malware, são programas desenvolvidos para executar ações danosas e atividades maliciosas em equipamentos, como computadores, modems, switches, roteadores e dispositivos móveis (tablets, celulares, smartphones, etc).Um atacante pode instalar um código malicioso após invadir um equipamento ou explorando alguma vulnerabilidade existente nos programas nele instalados.Seus equipamentos também podem ser infectados caso você:acesse páginas web maliciosas, usando navegadores vulneráveis", selfLayout: true,
+                    },
+                    textoForteDois: {
+                        texto: "CÓDIGOS MALICIOSOS: PROTEJA-SE DESTA TURMA", selfLayout: true,
+                    }
                 }
             ]
         }
@@ -29,49 +35,41 @@ export default {
 <style scoped lang="scss">
 .sessao-Dois{
     width: 100%;
-    height: 100%;
+    height: 1400px;
+    padding: 40px 0px;
     margin: 230px auto 0px auto;
+    background-color: rgba(255, 0, 0, 0.3);
+    
     .container-conteudo{
-        display: flex;
-        flex-wrap: wrap;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
         width: 600px;
+        height: 600px;
         margin: 0 auto;
-        .texto{
-            text-align: center;
-            font-size: 25px;
-        }
-        .titulo-normal{
-            text-align: center;
-            font-size: 40px;
-
-        }
+        place-items: center start;
+        gap:20px;
         .titulo-vermelho{
             color: #DA4731;
-            font-size: 35px;
-            width: 600px;
+            font-size: 40px;
+            width: 350px;
             margin: 0 auto;
-            text-align: center;
+            text-align: start;
+            place-self: start end; 
         }
-        .container-imgs{
-            width: 900px;
-            height: 100%;
+        .self-start-end{
+            place-self: end start;
             position: relative;
-            margin: 0 auto;
-            img{
-                max-width: 100%;
-                display: block
-            }
-            .mascara-img{
-                position: absolute;
-                left: 0px;
-                top: 0px;
-                z-index: 2;
-            }
-            .img-sessaoUm{
-                position: relative;
-                z-index: 1;
-                top: 100px;
-            }
+            top: 70px; 
+        }
+        .texto{
+            text-align: start;
+            font-size: 24px;
+            width: 320px;
+            height: 600px;
+        }
+        .position-self-down{
+            position: relative;
+            top: 70px;
         }
     }
 }
